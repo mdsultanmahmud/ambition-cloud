32b043e8e553e717a82f5ed8eefbfd150a66ca58
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2024-08-17 10:34:16 +0600

    navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+import { ImCross } from "react-icons/im";
+
 const Navbar = () => {
+  const [showNav, setShowNav] = useState(false);
+  const menu = (
+    <>
+      <li className="active">পােকজ</li>
+      <li className="">শতবলী</li>
+      <li>
+        <Link to={"/about"}>আমােদর সেক</Link>
+      </li>
+      <li className="">
+        <Link to={"/contact"}>যাগােযাগ</Link>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <h1>this is our navbar</h1>
+    <div className="ambition__cloud__navbar__container">
+      <div className="ambition__cloud__navbar__lg">
+        <div className="ambition__cloud__navbar__logo">
+          <Link to={"/"}>Ambition Cloud</Link>
+        </div>
+        <ul className="ambition__cloud__navbar__menu">{menu}</ul>
+        <div className="ambition__cloud__navbar__button">
+          <button>অডার কন</button>
+          <div className="ambition__cloud__navbar__toggle">
+            {!showNav ? (
+              <FaBars onClick={() => setShowNav(true)} />
+            ) : (
+              <ImCross onClick={() => setShowNav(false)} />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className={`ambition__cloud__navbar__sm ${showNav ? "active" : ""}`}>
+        <ul className="ambition__cloud__navbar__menu__sm">{menu}</ul>
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ const HomePage = () => {
   return (
     <div>
       <p>this is our home page</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laborum maiores ratione quia assumenda alias facilis, corporis voluptatem suscipit doloremque dolorem explicabo expedita quo libero deserunt, excepturi modi ad, ea inventore sunt delectus quod animi? Cumque aliquid soluta excepturi obcaecati ex perspiciatis consequuntur quas laborum qui necessitatibus nesciunt doloribus dolorem neque consectetur amet sunt animi, quos fugit dolorum illo est eius ducimus. Earum, molestiae? Corrupti, magni architecto. Vero assumenda, nisi reprehenderit aut fugit, molestias, velit nemo atque earum odit dolor delectus aliquid dignissimos beatae reiciendis cum! Esse fugit ea, neque accusantium nesciunt temporibus. Temporibus unde facere blanditiis sunt voluptatibus. Mollitia?</p>
     </div>
   );
 };
