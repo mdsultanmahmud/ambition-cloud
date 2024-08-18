b8252c902a70fc165458c65fff7dd5e1cbffedb7
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2024-08-18 16:18:39 +0600

    fix bug in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
+  const location = useLocation();
+  const [section, setSection] = useState();
   const [showNav, setShowNav] = useState(false);
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
   const menu = (
     <>
-      <li onClick={() => scrollToSection('package__section')} className="active">পােকজ</li>
-      <li onClick={() => scrollToSection('condition_section')} className="">শতবলী</li>
-      <li>
+      <li
+        onClick={() => {
+          scrollToSection("package__section");
+          setSection("package__section");
+        }}
+        className={`${
+          section === "package__section" &&
+          location.pathname === "/" &&
+          "active"
+        }`}
+      >
+        পােকজ
+      </li>
+      <li
+        onClick={() => {
+          scrollToSection("condition_section");
+          setSection("condition_section");
+        }}
+        className={`${
+          section === "condition_section" &&
+          location.pathname === "/" &&
+          "active"
+        }`}
+      >
+        শতবলী
+      </li>
+      <li className={`${location.pathname === "/about" && "active"}`}>
         <Link to={"/about"}>আমােদর সেক</Link>
       </li>
-      <li className="">
+      <li className={`${location.pathname === "/contact" && "active"}`}>
         <Link to={"/contact"}>যাগােযাগ</Link>
       </li>
     </>
@@ -32,7 +58,9 @@ const Navbar = () => {
         </div>
         <ul className="ambition__cloud__navbar__menu">{menu}</ul>
         <div className="ambition__cloud__navbar__button">
-          <button onClick={() => scrollToSection('order__section')}>অডার কন</button>
+          <button onClick={() => scrollToSection("order__section")}>
+            অডার কন
+          </button>
           <div className="ambition__cloud__navbar__toggle">
             {!showNav ? (
               <FaBars onClick={() => setShowNav(true)} />
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -42,7 +42,6 @@ const About = () => {
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14606.166771880598!2d90.37787904702067!3d23.763717395657263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c75f68e3e199%3A0x1091c4aa2634b568!2sTejgaon%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1723956727997!5m2!1sen!2sbd"
             style={{border:"0px"}}
-            allowfullscreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
